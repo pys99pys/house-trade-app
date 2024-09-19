4a0d0ca7c35c08f59fda752d5d6e5e4ee2575c38
박윤서 <ys@bag-yunseoui-MacBookPro.local>
2024-09-20 00:35:33 +0900

    저장 목록 컴포넌트 수정

--- app/saved-list/page.tsx
+++ app/saved-list/page.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 import Layout from "@/components/common/layout/Layout";
 import SavedList from "@/components/saved-list/SavedList";
 
 const Page = () => {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    setIsReady(true);
-  }, []);
-
-  if (!isReady) {
-    return null;
-  }
-
   return (
     <Layout>
       <SavedList />
