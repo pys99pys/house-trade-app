746d0999c41f235279b8c1061664edf2fc50f955
박윤서 <ys@bag-yunseoui-MacBookPro.local>
2024-09-30 01:09:05 +0900

    레이아웃 수정

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,11 +1,9 @@
-import cx from "classnames";
-import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { FC, ReactNode, useEffect, useState } from "react";
 import { FaRegBuilding } from "react-icons/fa";
 import { RecoilRoot } from "recoil";
 
-import { SAVED_LIST_PATH, TRADE_LIST_PATH } from "@/constants/paths";
+import { TRADE_LIST_PATH } from "@/constants/paths";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import styles from "./Layout.module.css";
@@ -15,7 +13,6 @@ interface LayoutProps {
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  const pathname = usePathname();
   const [isClient, setIsClient] = useState(false);
   const { push } = useRouter();
 
@@ -29,20 +26,10 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <QueryClientProvider client={new QueryClient()}>
       <RecoilRoot>
         <header className={styles.header}>
-          <div className={styles.container}>
-            <h1 onClick={() => push(TRADE_LIST_PATH)}>
-              <FaRegBuilding className={styles.logo} />
-              <span className={styles.text}>아파트 실거래가 조회</span>
-            </h1>
-            <nav>
-              <Link href={TRADE_LIST_PATH} className={cx({ [styles.active]: pathname === TRADE_LIST_PATH })}>
-                실거래가 조회
-              </Link>
-              <Link href={SAVED_LIST_PATH} className={cx({ [styles.active]: pathname === SAVED_LIST_PATH })}>
-                저장 목록
-              </Link>
-            </nav>
-          </div>
+          <h1 onClick={() => push(TRADE_LIST_PATH)}>
+            <FaRegBuilding className={styles.logo} />
+            <span className={styles.text}>아파트 실거래가 조회</span>
+          </h1>
         </header>
         <main className={styles.main}>{children}</main>
       </RecoilRoot>
