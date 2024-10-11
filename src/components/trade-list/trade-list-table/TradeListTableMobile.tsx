26afc0e0c2e3108d3aab9bb34055d0e8bc416530
박윤서 <ys@bag-yunseoui-MacBookPro.local>
2024-10-11 11:21:06 +0900

    모바일 저장 기능 추가

--- src/components/trade-list/trade-list-table/TradeListTableMobile.tsx
+++ src/components/trade-list/trade-list-table/TradeListTableMobile.tsx
@@ -1,3 +1,4 @@
+import cx from "classnames";
 import { FC } from "react";
 
 import Loading from "@/components/common/loading/Loading";
@@ -5,6 +6,7 @@ import Pagination from "@/components/common/pagination/Pagination";
 import Select from "@/components/common/select/Select";
 import { TRADE_TABLE_PER_PAGE } from "@/constants/rules";
 import { OrderType } from "@/interfaces/Order";
+import { createApartItemKey } from "@/utils/apartListUtil";
 import { parseToAmount, parseToFlatSize } from "@/utils/formatter";
 
 import styles from "./TradeListTableMobile.module.css";
@@ -40,7 +42,19 @@ const ORDER_OPTIONS: { value: OrderType[0]; label: string }[] = [
 ];
 
 const TradeListTableMobile: FC<TradeListTableMobileProps> = () => {
-  const { status, order, count, page, tradeList, onChangeOrder, onChangePage } = useTradeListTable();
+  const {
+    status,
+    order,
+    count,
+    page,
+    tradeList,
+    apartList,
+
+    onSaveItem,
+    onRemoveItem,
+    onChangeOrder,
+    onChangePage,
+  } = useTradeListTable();
 
   return (
     <div className={styles.TradeListTableMobile}>
@@ -63,42 +77,63 @@ const TradeListTableMobile: FC<TradeListTableMobileProps> = () => {
             <Loading />
           </div>
         )}
+
         {status === "EMPTY" && <div className={styles.empty}>데이터 없음</div>}
+
         {status === "SUCCESS" && (
           <div className={styles.list}>
             <ul>
-              {tradeList.map((item, i) => (
-                <li key={i}>
-                  <div className={styles.left}>
-                    <p className={styles.apartName}>
-                      {item.apartName}
-                      {item.size && <>({parseToFlatSize(item.size)}평)</>}
-                    </p>
-                    <p className={styles.address}>{item.address}</p>
-                    <p className={styles.etc}>
-                      {(() => {
-                        const subTexts: string[] = [];
+              {tradeList.map((item, i) => {
+                const isSavedItem = apartList.some((apartItem) => apartItem === createApartItemKey(item));
 
-                        if (item.floor !== null) subTexts.push(`${item.floor}층`);
-                        if (item.buildedYear !== null) subTexts.push(`${item.buildedYear}년식`);
-                        if (item.householdsNumber !== null) subTexts.push(`${item.householdsNumber}세대`);
+                return (
+                  <li key={i} className={cx(styles.row, { [styles.active]: isSavedItem })}>
+                    <div className={styles.left}>
+                      <p className={styles.apartName}>
+                        {item.apartName}
+                        {item.size && <>({parseToFlatSize(item.size)}평)</>}
+                      </p>
+                      <p className={styles.address}>{item.address}</p>
+                      <p className={styles.etc}>
+                        {(() => {
+                          const subTexts: string[] = [];
 
-                        return subTexts.length > 0 ? subTexts.join("/") : "";
-                      })()}
-                    </p>
-                    <p className={styles.tradeDate}>{item.tradeDate}</p>
-                  </div>
-                  <div className={styles.right}>
-                    <p className={styles.tradeAmount}>
-                      {parseToAmount(item.tradeAmount)}억원{item.isNewRecord && "(신)"}
-                    </p>
-                    {item.maxTradeAmount > 0 && <p>{parseToAmount(item.maxTradeAmount)}억원</p>}
-                  </div>
-                </li>
-              ))}
+                          if (item.floor !== null) subTexts.push(`${item.floor}층`);
+                          if (item.buildedYear !== null) subTexts.push(`${item.buildedYear}년식`);
+                          if (item.householdsNumber !== null) subTexts.push(`${item.householdsNumber}세대`);
+
+                          return subTexts.length > 0 ? subTexts.join("/") : "";
+                        })()}
+                      </p>
+                      <p className={styles.tradeDate}>{item.tradeDate}</p>
+                    </div>
+                    <div className={styles.right}>
+                      <div className={styles.amountWrap}>
+                        <p className={styles.tradeAmount}>
+                          {parseToAmount(item.tradeAmount)}억원{item.isNewRecord && "(신)"}
+                        </p>
+                        {item.maxTradeAmount > 0 && <p>{parseToAmount(item.maxTradeAmount)}억원</p>}
+                      </div>
+                      <div className={styles.buttonWrap}>
+                        {isSavedItem && (
+                          <button className={styles.removeButton} onClick={() => onRemoveItem(item)}>
+                            삭제
+                          </button>
+                        )}
+                        {!isSavedItem && (
+                          <button className={styles.saveButton} onClick={() => onSaveItem(item)}>
+                            저장
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         )}
+
         {TRADE_TABLE_PER_PAGE > 0 && (
           <div className={styles.pagenationWrap}>
             <Pagination
